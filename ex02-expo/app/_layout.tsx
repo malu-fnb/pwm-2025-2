de3bd00b166fa3171d0d6208b4c0bbb726c3f52d
malu-fnb <134156256+malu-fnb@users.noreply.github.com>
2025-10-28 01:48:50 +0000

    novo tema

--- ex02-expo/app/_layout.tsx
+++ ex02-expo/app/_layout.tsx
@@ -1,12 +1,28 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
 
 const queryClient = new QueryClient();
 
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#00695C", 
+    secondary: "#00BFA5", 
+  },
+};
+
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack />
-    </QueryClientProvider>
+    <PaperProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Stack />
+      </QueryClientProvider>
+    </PaperProvider>
   );
 }
